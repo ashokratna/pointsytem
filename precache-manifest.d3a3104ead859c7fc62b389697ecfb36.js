b28142bd3b51189f17417e53464833f92a00514d
Ankur  Pandey <UP0419@uplers.local>
2019-12-24 15:02:14 +0530

    Updates

---
+++ precache-manifest.d3a3104ead859c7fc62b389697ecfb36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c35a2b9717da1f6b16303f01f4d43e",
+    "url": "/pointsytem/index.html"
+  },
+  {
+    "revision": "4e87342fcf55b395a5ba",
+    "url": "/pointsytem/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "86ff7da27acefbbf3989",
+    "url": "/pointsytem/static/css/main.fdb97f00.chunk.css"
+  },
+  {
+    "revision": "4e87342fcf55b395a5ba",
+    "url": "/pointsytem/static/js/2.5ac5b655.chunk.js"
+  },
+  {
+    "revision": "b6db7fb8eead474a1aada3d9a1b57712",
+    "url": "/pointsytem/static/js/2.5ac5b655.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86ff7da27acefbbf3989",
+    "url": "/pointsytem/static/js/main.251cd96f.chunk.js"
+  },
+  {
+    "revision": "99eaeeffaabb548c4f7a",
+    "url": "/pointsytem/static/js/runtime-main.eb1edb45.js"
+  }
+]);
